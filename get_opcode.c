d751ada5c84d3a17b775cdd73dce940f0c523b70
Valentin <5619@holbertonstudents.com>
2022-12-12 09:36:02 -0800

    function who call the opcode

---
+++ get_opcode.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * get_opcode - call the functione opcode in the monty file
+ * @buf: string in the monty file
+ * @head: double pointer of the linked list (next&prev)
+ * @line_number: line of the file
+ * Return: 0 if succes
+ */
+
+
+int get_opcode(char *buf, stack_t **head, unsigned int line_number)
+{
+	/* struct who call the function */
+	instruction_t func[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (func[i].opcode != NULL) /*loop for call the function */
+	{
+		/* if the name of the function is 0 call the func */
+		if (strcmp(func[i].opcode, buf) == 0)
+			return ((int)((func[i].f)(head, line_number))); /* return the function */
+		i++;
+	}
+return (0);
+}
