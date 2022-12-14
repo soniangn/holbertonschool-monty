087d600006629b1e269d12eb1601f37bf5de6bf3
Valentin <5619@holbertonstudents.com>
2022-12-14 07:35:25 -0800

    add pall.c

---
+++ pall.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * op_pall - function that print all the values on the stack
+ * @top: double pointer to the stack
+ * @line_number: the line number where the opcode is called
+ */
+void op_pall(stack_t **top, unsigned int line_number)
+{
+	stack_t *temp;
+	(void)line_number;
+
+	temp = *top;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	free(temp);
+}
