5cb9c7acba5192a5ac6a796946c35fbde2eb80a8
Valentin <5619@holbertonstudents.com>
2022-12-16 03:03:45 -0800

    op_push.c

--- main.c
+++ main.c
@@ -3,15 +3,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+char *value;
 /**
  * main - entry point
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: 0
  */
-
-char *value;
-
 int main(int argc, char *argv[])
 {
 	FILE *fp;
